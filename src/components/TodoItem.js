3c1af36323494cbe0f2d0d6820d29fdb65dbd5a4
Erick Maeda <erick.maeda26@gmail.com>
2019-02-25 12:37:50 -0300

    Adicionado style para tasks finalizadas

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -14,16 +14,28 @@ import {
 
 class TodoItem extends Component {
 
-    render() {        
-        const {description, completed, id} = this.props.todo;
+    renderDescription = () => {
+        const {completed, description} = this.props.todo;
+        const style = completed ? styleDescriptionCompleted : {};
+        return (
+            <span style={style}>{description}</span>
+        );
+    }
+
+    render() {
+        const {completed, id} = this.props.todo;
         return (
             <Card>
                 <Card.Body>
-                    <FormCheck onChange={this.props.updateCompleted.bind(this, id, !completed)} checked={completed} inline />{description}
+                    <FormCheck onChange={this.props.updateCompleted.bind(this, id, !completed)} checked={completed} inline />{this.renderDescription()}
                 </Card.Body>
             </Card>
         );
     }
+};
+
+const styleDescriptionCompleted = {
+    textDecoration: 'line-through'
 };
 
 export default connect(null, {updateCompleted})(TodoItem);
